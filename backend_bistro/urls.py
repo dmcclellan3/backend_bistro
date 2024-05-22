2c50a73510e810bfab9cf4d0d9349708f0b99b81
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-22 15:22:01 -0400

    save

--- backend_bistro/urls.py
+++ backend_bistro/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from backend_bistro_app.views import CategoryViewSet, MenuItemViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'menuitems', MenuItemViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('backend_bistro.urls')),
+# ]
